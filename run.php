667f0e2d515d0cfbe621bbd71097f10001694d89
Christian Shungu <christianshungu@gmail.com>
2024-03-03 18:04:05 +0100

    Comprehension du tri par Insertion

--- run.php
+++ run.php
@@ -1,5 +1,4 @@
 <?php 
-
 /**
  * Function triByInsertAsc
  * 
@@ -7,7 +6,41 @@
  * 
  * @return array
  */
-function triByInsertAsc() : array
+function triByInsertAsc(array $ints) : array
 {
-    return [];
+    $cardinal = count($ints);
+    for ($j=1; $cardinal> $j; $j++) {
+        $key = $ints[$j];
+        $i= $j - 1;
+        while ($i>-1 && $ints[$i] >$key) {
+            $ints[$i + 1] = $ints[$i];
+            $i = $i - 1;
+        }
+        $ints[$i + 1] = $key;
+    }
+    return $ints;
 }
+/**
+ * Function triByInsertDesc
+ * 
+ * @param  array $ints
+ * 
+ * @return array
+ */
+function triByInsertDesc(array $ints) : array 
+{
+
+    $cardinal = count($ints);
+    for ($j=1; $cardinal> $j; $j++) {
+        $key = $ints[$j];
+        $i= $j - 1;
+        while ($i>-1 && $ints[$i] < $key) {
+            $ints[$i + 1] = $ints[$i];
+            $i = $i - 1;
+        }
+        $ints[$i + 1] = $key;
+    }    return $ints;
+}
+$ints = [31,41,59,26,41, 58]; 
+print_r(triByInsertAsc($ints)); // [26, 31, 41, 41, 58]
+print_r(triByInsertDesc($ints)); // [58, 41, 41, 31, 26]
